ce6f5f60423df06b6d1c78e32e984581967e209f
daisaja <daisaja@gmx.de>
2010-06-08 13:51:29 +0200

    My eclipse TaskIt project.

---
+++ src/de/daisaja/android/taskit/TaskIt.java
@@ -0,0 +1,13 @@
+package de.daisaja.android.taskit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TaskIt extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
