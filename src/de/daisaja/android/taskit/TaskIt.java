204316eb7725c1ba66c400a566faf67ad0d3030f
daisaja <daisaja@gmx.de>
2010-06-08 15:15:34 +0200

    First layouts.

--- src/de/daisaja/android/taskit/TaskIt.java
+++ src/de/daisaja/android/taskit/TaskIt.java
@@ -1,13 +1,38 @@
 package de.daisaja.android.taskit;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class TaskIt extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class TaskIt extends ListActivity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.relative);
+
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.taskit_start,
+				new String[] { "123", "456", "789" }));
+
+		ListView listView = getListView();
+		listView.setTextFilterEnabled(true);
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// When clicked, show a toast with the TextView text
+				Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		// registerForContextMenu(getListView());
+
+	}
 }
